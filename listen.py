e289fff31f6ff2e448734bf632cf0821208e3fc2
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-25 19:00:55 +0200

    feature(Listen): Print out status

--- listen.py
+++ listen.py
@@ -1,26 +1,26 @@
-## listen.py
+import RPi.GPIO as GPIO
+import sys, json, time
 
-import sys, json, numpy as np
+print("Starting Sensor")
+ 
+sensor = 4
 
-#Read data from stdin
-def read_in():
-    lines = sys.stdin.readlines()
-    #Since our input would only be having one line, parse our JSON data from that
-    return json.loads(lines[0])
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
-def main():
-    #get our data as an array from read_in()
-    lines = read_in()
+previous_state = False
+current_state = False
 
-    #create a numpy array
-    np_lines = np.array(lines)
+def listener() :
 
-    #use numpys sum method to find sum of all elements in the array
-    lines_sum = np.sum(np_lines)
+    while True:
+        time.sleep(0.1)
+        previous_state = current_state
+        current_state = GPIO.input(sensor)
+        if current_state:
+            print("Team : %s" % (sensor)) 
+        else: 
+            print("Clear")
 
-    #return the sum to the output stream
-    print lines_sum
-
-#start process
-if __name__ == '__main__':
-    main()
+if __name__ =='__main__' :
+    listener = listener()
--- start.js
+++ start.js
@@ -1,29 +1,23 @@
 //start.js
 var spawn = require('child_process').spawn,
-    py    = spawn('python', ['listen.py']),
-    data = [1,2,3,4,5,6,7,8,9],
-    dataString = '';
+    py    = spawn('python', ['-u', 'listen.py']);
 
+/*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), 
+we want to convert that received data into a string and append it to the overall dataString.*/
 py.stdout.on('data', function(data){
-  dataString += data.toString();
+  console.log(data.toString());
 });
+
+/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
 py.stdout.on('end', function(){
-  console.log('Sum of numbers=',dataString);
+  console.log('Listening to sensor stopped');
 });
-py.stdin.write(JSON.stringify(data));
-py.stdin.end();
 
+py.stderr.on('data', function(data) {
+    console.log('stderr: ' + data);
+    // Error occurred;
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+py.on('close', function(code) {
+    console.log('child process exited with code ' + code);
+});
