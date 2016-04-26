4911d6717c0746dbe008226a7c1ee47cd65ab962
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-26 20:36:14 +0200

    create server template

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+var spawn = require('child_process').spawn,
+    exec = require('child_process').exec,
+    child;
+
+app.use(express.static(__dirname + '/public'));  
+
+app.get('/', function(req, res,next) {  
+    res.sendFile(__dirname + 'index.html');
+});
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+    socket.on('doRestart', function (data) {
+	console.log('Restarting...');
+        io.sockets.emit('restart', { }); 
+    });
+});
+
+console.log("Listening on port " + port);
+
+child = spawn('node', ['start.js']);
+
+child.stdout.on('data', function (data) {
+	console.log('Child says: ' + data);
+});
