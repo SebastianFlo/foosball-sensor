9f926adfacc53706f459d68b54dcaa31bbd11097
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-27 21:28:33 +0200

    feature(client): Add js

---
+++ views/js/client.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+ 
+    var messages = [];
+    var socket = io.connect('http://localhost:3700');
+ 
+    socket.on('message', function (data) {
+        if(data.message) {
+            console.log(data);
+            messages.push(data);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+            }
+            content.innerHTML = html;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+}
