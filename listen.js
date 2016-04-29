5fbb27701c5efcb6b553f79984fc2370c812a5b4
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-29 17:45:32 +0200

    chore(Listen): Move sever

--- listen.js
+++ listen.js
@@ -30,32 +30,7 @@ io.sockets.on('connection', function(socket)
 
 /*Here we are saying that every time our node application receives data from the python process output stream(on 'data'), 
 we want to convert that received data into a string and append it to the overall dataString.*/
-child.stdout.on('data', function(data){
-  var result = data.toString();
-  var triggeredSensor = result.split(':')[1];
-  if (triggeredSensor) {
-    console.log("Scored by team", triggeredSensor);
-  }
-});
 
-/*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
-child.stdout.on('end', function(){
-  console.log('Listening to sensor stopped');
-});
-
-child.stderr.on('data', function(data) {
-    console.log('stderr: ' + data);
-    // Error occurred;
-});
-
-child.on('close', function(code) {
-    console.log('child process exited with code ' + code);
-});
-
-server.listen(8124, function() { //'listening' listener
-  console.log('server bound');
-  child = spawn('python', ['-u', 'listen.py']);
-});
 
 var net = require('net');
 var server = net.createServer(function(connection) { //'connection' listener
@@ -65,7 +40,6 @@ var server = net.createServer(function(connection) { //'connection' listener
   });
   
   connection.on('data', function(data){
-  
       io.sockets.emit('data', data);
       console.log('data', data);
   
@@ -73,6 +47,34 @@ var server = net.createServer(function(connection) { //'connection' listener
 
 });
 
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+  child = spawn('python', ['-u', 'listen.py']);
+  
+  child.stdout.on('data', function(data){
+    var result = data.toString();
+    var triggeredSensor = result.split(':')[1];
+    if (triggeredSensor) {
+      console.log("Scored by team", triggeredSensor);
+    }
+  });
+
+  /*Once the stream is done (on 'end') we want to simply log the received data to the console.*/
+  child.stdout.on('end', function(){
+    console.log('Listening to sensor stopped');
+  });
+
+  child.stderr.on('data', function(data) {
+      console.log('stderr: ' + data);
+      // Error occurred;
+  });
+
+  child.on('close', function(code) {
+      console.log('child process exited with code ' + code);
+  });
+  
+  
+});
 
 
 
