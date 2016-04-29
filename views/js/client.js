16d8c35168c85b7e587976a9719c7a3c0a1fd084
Sebastian Florian <rebardesigndk@gmail.com>
2016-04-29 18:26:17 +0200

    chore(Client): Log data

--- views/js/client.js
+++ views/js/client.js
@@ -3,14 +3,9 @@ window.onload = function() {
     var messages = [];
     var socket = io.connect();
  
-    socket.on('message', function (data) {
-        if(data.message) {
+    socket.on('goal', function (data) {
+        if(data) {
             console.log(data);
-            messages.push(data);
-            var html = '';
-            for(var i=0; i<messages.length; i++) {
-            }
-            content.innerHTML = html;
         } else {
             console.log("There is a problem:", data);
         }
