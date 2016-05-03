97a8b1763060219adf12ac54e563c4b1c5ff70b0
Sebastian Florian <rebardesigndk@gmail.com>
2016-05-03 14:08:36 +0200

    chore(listen): use timer

--- listen.py
+++ listen.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 import sys, json, time
+from decimal import *
 
 print("Starting Sensor")
  
@@ -18,12 +19,10 @@ def listener() :
         time.sleep(0.01)
         previous_state = current_state
         current_state = GPIO.input(sensor)
-        # if laser is triggered
-        if not current_state:
-            # start timer
-            start = time.time()
-        # if was triggered by not anymore
-        if not previous_state and current_state 
+        # start timer
+        start = time.time()
+        # if sensor was triggered by not anymore
+        if not previous_state and current_state:
             end = time.time()
             duration = end - start
             print("Team : %s : %s" % (sensor, duration)) 
