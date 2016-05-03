af9c1252f783d6ac27e3593dd546ff6d426aabeb
Sebastian Florian <rebardesigndk@gmail.com>
2016-05-03 17:36:50 +0200

    chore(Server): Use prev time

--- server.js
+++ server.js
@@ -20,12 +20,12 @@ child = spawn('python', ['-u', 'listen.py']);
 io.sockets.on('connection', function(socket)
 {
     
-    var startTime, stopTime;
+    var startTime, prevTime;
     child.stdout.on('data', function(data){
         startTime = Math.floor(Date.now() / 1000);
         console.log('Start Time', startTime);
         // set debounce
-        if (startTime - stopTime > 0.5) {
+        if (startTime - prevTime > 0.5) {
             var result = data.toString();
             var triggeredSensor = result.split(':')[0];
             var speed = result.split(':')[1];
@@ -36,10 +36,10 @@ io.sockets.on('connection', function(socket)
                 }
                 io.sockets.emit('goal', goal);
                 // setting a data send debounce
-                stopTime = Math.floor(Date.now() / 1000);
                 console.log('Stop Time', stopTime);
             }
         }
+        prevTime = startTime;
     });
   
     child.on('close', function(code) {
