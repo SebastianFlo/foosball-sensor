6370fb70253b7d2d0be2815c96d28f697500b66f
Sebastian Florian <rebardesigndk@gmail.com>
2016-05-03 17:31:50 +0200

    chore(server): Start stop time on server

--- server.js
+++ server.js
@@ -20,20 +20,25 @@ child = spawn('python', ['-u', 'listen.py']);
 io.sockets.on('connection', function(socket)
 {
     
+    var startTime, stopTime;
     child.stdout.on('data', function(data){
-        var result = data.toString();
-        var triggeredSensor = result.split(':')[0];
-        var speed = result.split(':')[1];
-        if (speed) {
-            var goal = {
-                team: triggeredSensor,
-                speed: speed
+        startTime = Math.floor(Date.now() / 1000);
+        console.log('Start Time', startTime);
+        // set debounce
+        if (startTime - stopTime > 0.5) {
+            var result = data.toString();
+            var triggeredSensor = result.split(':')[0];
+            var speed = result.split(':')[1];
+            if (speed) {
+                var goal = {
+                    team: triggeredSensor,
+                    speed: speed
+                }
+                io.sockets.emit('goal', goal);
+                // setting a data send debounce
+                stopTime = Math.floor(Date.now() / 1000);
+                console.log('Stop Time', stopTime);
             }
-            io.sockets.emit('goal', goal);
-            // setting a data send debounce
-            setTimeout(function(){
-                
-            }, 500);
         }
     });
   
