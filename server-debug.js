021192553eea78b5802a888ab2a9280976d02409
Sebastian Florian <rebardesigndk@gmail.com>
2016-05-09 14:21:30 +0200

    chore(debug)

--- server-debug.js
+++ server-debug.js
@@ -3,7 +3,6 @@ var app = express();
 var port = 3700;
 var debounce = require('debounce');
 var spawn = require('child_process').spawn,
-    exec = require('child_process').exec,
     child;
 
 app.use(express.static(__dirname + '/views'));  
@@ -22,34 +21,11 @@ console.log("Listening on port " + port);
 child1 = spawn('python', ['-u', 'listen-debug1.py']);
 child2 = spawn('python', ['-u', 'listen-debug2.py']);
 
+child1.stdout.on('data', debugData);
+child2.stdout.on('data', debugData);
+
 io.sockets.on('connection', function(socket)
 {
-    
-    child1.stdout.on('data', debugData);
-    child2.stdout.on('data', debugData);
-    
-    function debugData(data) {
-        console.log(data.toString());
-        // var result = data.toString();
-        // var id = result.split(':')[0];
-        // console.log(id);
-    }
-    
-    function emitGoal(data){
-        // set debounce
-        var result = data.toString();
-        var id = result.split(':')[0];
-        var speed = result.split(':')[1];
-        if (speed) {
-            var goal = {
-                team: id,
-                speed: speed
-            }
-            console.log('Team ' + goal.team + ' scores');
-            io.sockets.emit('goal', goal);
-        }
-    }
-  
     child1.on('close', function(code) {
       console.log('child 1 process exited with code ' + code);
     });
@@ -60,3 +36,24 @@ io.sockets.on('connection', function(socket)
   
 }); // end on connection
 
+function emitGoal(data){
+    // set debounce
+    var result = data.toString();
+    var id = result.split(':')[0];
+    var speed = result.split(':')[1];
+    if (speed) {
+        var goal = {
+            team: id,
+            speed: speed
+        }
+        console.log('Team ' + goal.team + ' scores');
+        io.sockets.emit('goal', goal);
+    }
+}
+
+function debugData(data) {
+    console.log(data.toString());
+    // var result = data.toString();
+    // var id = result.split(':')[0];
+    // console.log(id);
+}
